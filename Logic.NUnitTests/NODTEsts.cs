958890564d7f39a7f718858a47a32a45290296ec
adaniluk <allldaniluk@gmail.com>
2017-07-08 19:18:53 +0300

    Added 'Nod' class

---
+++ Logic.NUnitTests/NODTEsts.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using Logic;
+
+namespace Logic.NUnitTests
+{
+    [TestFixture]
+    public class NodTests
+    {
+        #region Euclid
+        [TestCase(12, 16, ExpectedResult = 4)]
+        [TestCase(12, 0, ExpectedResult = 12)]
+        [TestCase(0, 16, ExpectedResult = 16)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(-12, 16, ExpectedResult = 4)]
+        [TestCase(12, -16, ExpectedResult = 4)]
+        [TestCase(-12, -16, ExpectedResult = 4)]
+        [TestCase(12, 16, 11, ExpectedResult = 1)]
+        [TestCase(12, 16, 36, ExpectedResult = 4)]
+        [TestCase(0, 16, 36, ExpectedResult = 4)]
+        [TestCase(12, 0, 36, ExpectedResult = 12)]
+        [TestCase(0, 16, 0, ExpectedResult = 16)]
+        [TestCase(0,0,0,0,0, ExpectedResult = 0)]
+        public int Euclid_CorrectValues_PositiveTest(params int[] array)
+        { 
+            int time;
+            return Nod.Euclid(out time, array);
+        }
+        #endregion
+
+        #region Stein
+        [TestCase(12, 16, ExpectedResult = 4)]
+        [TestCase(12, 0, ExpectedResult = 12)]
+        [TestCase(0, 16, ExpectedResult = 16)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(-12, 16, ExpectedResult = 4)]
+        [TestCase(12, -16, ExpectedResult = 4)]
+        [TestCase(-12, -16, ExpectedResult = 4)]
+        [TestCase(12, 16, 11, ExpectedResult = 1)]
+        [TestCase(12, 16, 36, ExpectedResult = 4)]
+        [TestCase(0, 16, 36, ExpectedResult = 4)]
+        [TestCase(12, 0, 36, ExpectedResult = 12)]
+        [TestCase(0, 16, 0, ExpectedResult = 16)]
+        [TestCase(0, 0, 0, 0, 0, ExpectedResult = 0)]
+        public int Stein_CorrectValues_PositiveTest(params int[] array)
+        {
+            int time;
+            return Nod.Stein(out time, array);
+        }
+        #endregion
+    }
+}
---
+++ Logic/NOD.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Logic
+{
+    public class Nod
+    {
+        #region Euclid
+        /// <summary>
+        /// Finds NOD using Euclid method.
+        /// </summary>
+        /// <param name="time">Time of execution program in milliseconds.</param>
+        /// <param name="array">Array of parameters.</param>
+        /// <returns>NOD of all parameters.</returns>
+        public static int Euclid(out int time, params int[] array)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int nod = Euclid(array[0], array[1]);
+            for (int i = 2; i < array.Length; i++)
+            {
+                nod = Euclid(array[i], nod);
+            }
+            stopwatch.Stop();
+            time = stopwatch.Elapsed.Milliseconds;
+            return nod;
+        }
+
+        private static int Euclid(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == 0) return b;
+            if (b == 0) return a;
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            return a;
+        }
+        #endregion
+
+        #region Stein
+        /// <summary>
+        /// Finds NOD using Stein method.
+        /// </summary>
+        /// <param name="array">Array of parameters.</param>
+        /// <returns>NOD of all parameters.</returns>
+        public static int Stein(out int time, params int[] array)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int nod = Stein(array[0], array[1]);
+            for (int i = 2; i < array.Length; i++)
+            {
+                nod = Stein(array[i], nod);
+            }
+            stopwatch.Stop();
+            time = stopwatch.Elapsed.Milliseconds;
+            return nod;
+        }
+
+        private static int Stein(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == b) return a;
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == 1 || b == 1) return 1;
+
+
+            //if ((a & 1) == 0) return ((b & 1) == 1) ? Stein(a >> 1, b) : (Stein(a >> 1, b >> 1) << 1);
+            //else return ((b & 1) == 0) ? Stein(a, b >> 1) : ((a < b) ? Stein(a, Math.Abs(a - b) >> 1) : Stein(Math.Abs(a - b) >> 1, b));
+
+            if ((a & 1) == 0)
+            {
+                if ((b & 1) == 1)
+                    return Stein(a >> 1, b);
+                else
+                    return (Stein(a >> 1, b >> 1) << 1);
+            }
+            else
+            {
+                if ((b & 1) == 0)
+                    return Stein(a, b >> 1);
+                else
+                {
+                    if (a < b)
+                        return Stein(a, Math.Abs(a - b) >> 1);
+                    else
+                        return Stein(Math.Abs(a - b) >> 1, b);
+                }
+            }
+        }
+        #endregion
+    }
+}
