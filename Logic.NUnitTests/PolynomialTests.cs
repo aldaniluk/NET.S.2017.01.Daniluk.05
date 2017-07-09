89a08e000dfdb289e0d4d87941b3fd5ffb501c65
adaniluk <allldaniluk@gmail.com>
2017-07-09 11:19:35 +0300

    Added 'Polynomial' class

---
+++ Logic.NUnitTests/PolynomialTests.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Logic;
+using NUnit.Framework;
+
+namespace Logic.NUnitTests
+{
+    [TestFixture]
+    class PolynomialTests
+    {
+        #region ToStringTest
+        [TestCase(new double[] { 2, 0, 1 }, ExpectedResult = "2 + 1*(x^2)")]
+        [TestCase(new double[] { 3, 5 }, ExpectedResult = "3 + 5*(x^1)")]
+        [TestCase(new double[] { 0, 0, 4, 6 }, ExpectedResult = "4*(x^2) + 6*(x^3)")]
+        public string ToStringTest_CorrectValues_PositiveTest(double[] array1)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            return p1.ToString();
+        }
+        #endregion
+
+        #region EqualsTest
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0, 1 }, ExpectedResult = true)]
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0, 2 }, ExpectedResult = false)]
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0 }, ExpectedResult = false)]
+        public bool EqualsTest_CorrectValues_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            return p1.Equals(p2);
+        }
+
+        [Test]
+        public void EqualsTest_NullInputValues_PositiveTest()
+        {
+            Polynomial p1 = new Polynomial(new double[] { 2, 0, 1 });
+            Polynomial p2 = null;
+            Assert.False(p1.Equals(p2));
+        }
+
+        [Test]
+        public void EqualsTest_SameInputValues_PositiveTest()
+        {
+            Polynomial p1 = new Polynomial(new double[] { 2, 0, 1 });
+            Polynomial p2 = p1;
+            Assert.True(p1.Equals(p2));
+        }
+        #endregion
+
+        #region operators +,-,*,==,!=
+        #region +
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 1, 4 }, new double[] { 3, 4, 1 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 1, 4 }, new double[] { 1, 4, 1 })]
+        public void OperatorPlus_CorrectValues_PositiveTest(double[] array1, double[] array2, double[] result)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial expected = new Polynomial(result);
+            Assert.True((p1+p2).Equals(expected));
+        }
+        #endregion
+
+        #region -
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 1, 4 }, new double[] { 1, -4, 1 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 1, 4 }, new double[] { -1, -4, 1 })]
+        public void OperatorMinus_CorrectValues_PositiveTest(double[] array1, double[] array2, double[] result)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial expected = new Polynomial(result);
+            Assert.True((p1 - p2).Equals(expected));
+        }
+        #endregion
+
+        #region *
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 1, 4 }, new double[] { 2, 8, 1, 4 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 1, 4 }, new double[] { 0, 0, 1, 4 })]
+        public void OperatorMultiply_CorrectValues_PositiveTest(double[] array1, double[] array2, double[] result)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Polynomial expected = new Polynomial(result);
+            Assert.True((p1 * p2).Equals(expected));
+        }
+        #endregion
+
+        #region ==
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0, 1 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 0, 0, 1 })]
+        [TestCase(new double[] { 0, 0, 0 }, new double[] { 0, 0, 0 })]
+        public void OperatorEqual_True_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Assert.True(p1 == p2);
+        }
+
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 0, 0, 2 })]
+        [TestCase(new double[] { 0, 0 }, new double[] { 0, 0, 0 })]
+        public void OperatorEqual_False_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Assert.False(p1 == p2);
+        }
+        #endregion
+
+        #region !=
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 0, 0, 2 })]
+        [TestCase(new double[] { 1, 0, 0 }, new double[] { 0, 0, 0 })]
+        public void OperatorNotEqual_True_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Assert.True(p1 != p2);
+        }
+
+        [TestCase(new double[] { 2, 0, 1 }, new double[] { 2, 0, 1 })]
+        [TestCase(new double[] { 0, 0, 1 }, new double[] { 0, 0, 1 })]
+        [TestCase(new double[] { 0, 0 }, new double[] { 0, 0 })]
+        public void OperatorNotEqual_False_PositiveTest(double[] array1, double[] array2)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            Polynomial p2 = new Polynomial(array2);
+            Assert.False(p1 != p2);
+        }
+        #endregion
+
+        #endregion
+
+        #region CalculateByValue
+        [TestCase(new double[] { 2, 0, 1 }, 2, ExpectedResult = 6)]
+        [TestCase(new double[] { 0, 0, 0 }, 2, ExpectedResult = 0)]
+        [TestCase(new double[] { 5, 4, 2, 11 }, 0, ExpectedResult = 5)]
+        [TestCase(new double[] { 2, 3 }, 3, ExpectedResult = 11)]
+        public double CalculateByValue_CorrectValues_PositiveTest(double[] array1, double value)
+        {
+            Polynomial p1 = new Polynomial(array1);
+            return p1.CalculateByValue(value);
+        }
+        #endregion
+    }
+}
---
+++ Logic/Polynomial.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Polynomial
+    {
+        #region public fields
+        /// <summary>
+        /// Max degree of polynomial.
+        /// </summary>
+        public int Degree { get; }
+        #endregion
+
+        #region private fields
+        /// <summary>
+        /// Array of coefficients.
+        /// </summary>
+        private СoefficientArray coefficientArray;
+        #endregion
+
+        #region struct СoefficientArray
+        /// <summary>
+        /// Struct for interact with an array of coefficients.
+        /// </summary>
+        private struct СoefficientArray
+        {
+            private double[] array;
+            public int Length;
+
+            public СoefficientArray(params double[] inputArray)
+            {
+                array = new double[inputArray.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = inputArray[i];
+                }
+                Length = array.Length;
+            }
+
+            public double[] GetArray()
+            {
+                double[] result = new double[array.Length];
+                array.CopyTo(result, 0);
+                return result;
+            }
+
+            public double this[int i]
+            {
+                get
+                {
+                    if(i < 0 || i >= Length) throw new ArgumentOutOfRangeException(nameof(i));
+                    return array[i];
+                }
+            }
+        }
+        #endregion
+
+        #region ctors
+        /// <summary>
+        /// Ctor for Polynomial instance.
+        /// </summary>
+        /// <param name="inputCoefficientArray">Double array of coefficients.</param>
+        public Polynomial(params double[] inputCoefficientArray)
+        {
+            if (inputCoefficientArray == null) throw new ArgumentNullException($"{ nameof(inputCoefficientArray) } is null.");
+            if (inputCoefficientArray.Length == 0) throw new ArgumentException($"{ nameof(inputCoefficientArray) } is empty.");
+
+            coefficientArray = new СoefficientArray(inputCoefficientArray);
+            Degree = coefficientArray.Length - 1;
+        }
+
+        /// <summary>
+        /// Ctor for Polynomial instance.
+        /// </summary>
+        /// <param name="polynomial">Polynomial instance.</param>
+        public Polynomial(Polynomial polynomial)
+        {
+            if (polynomial == null) throw new ArgumentNullException($"{ nameof(polynomial) } is null.");
+
+            coefficientArray = new СoefficientArray(polynomial.coefficientArray.GetArray());
+            Degree = coefficientArray.Length - 1;
+        }
+        #endregion
+
+        #region public methods
+
+        #region overrided methods
+        /// <summary>
+        /// Converts instance into string representation.
+        /// </summary>
+        /// <returns>String representation of an instance.</returns>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i=0; i< coefficientArray.Length; i++)
+            {
+                if (coefficientArray[i] == 0) continue;
+                if (coefficientArray[i] < 0) result.Append("(" + coefficientArray[i].ToString() + ")");
+                if (coefficientArray[i] > 0) result.Append(coefficientArray[i].ToString());
+                if (i != 0) result.Append("*" + "(x^" + i + ")");
+                if (i != coefficientArray.Length-1) result.Append(" + ");
+            }
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Checks for equality.
+        /// </summary>
+        /// <param name="obj">One of the objects.</param>
+        /// <returns>True, if objects are equal, and false otherwise.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null)) return false;
+            if (ReferenceEquals(obj, this)) return true;
+            return Equals(obj as Polynomial);
+        }
+        #endregion
+
+        #region overloaded operators
+        /// <summary>
+        /// Finds the sum of two polynomials.
+        /// </summary>
+        /// <param name="polynomial1">First polynomial.</param>
+        /// <param name="polynomial2">Second polynomial.</param>
+        /// <returns>Sum of two polynomials.</returns>
+        public static Polynomial operator + (Polynomial polynomial1, Polynomial polynomial2)
+        {
+            CheckInputPolynomials(polynomial1, polynomial2);
+
+            int sizeMax = (polynomial1.Degree >= polynomial2.Degree) ? polynomial1.Degree+1 : polynomial2.Degree+1;
+            double[] arrayResult = new double[sizeMax];
+
+            for (int i = 0; i < sizeMax; i++)
+            {
+                if(i >= polynomial1.coefficientArray.Length)
+                {
+                    arrayResult[i] += polynomial2.coefficientArray[i];
+                    continue;
+                }
+                if (i >= polynomial2.coefficientArray.Length)
+                {
+                    arrayResult[i] += polynomial1.coefficientArray[i];
+                    continue;
+                }
+                arrayResult[i] = polynomial1.coefficientArray[i] + polynomial2.coefficientArray[i];
+            }
+            
+            return new Polynomial(arrayResult);
+        }
+
+        /// <summary>
+        /// Finds the difference of two polynomials.
+        /// </summary>
+        /// <param name="polynomial1">First polynomial.</param>
+        /// <param name="polynomial2">Second polynomial.</param>
+        /// <returns>Difference of two polynomials.</returns>
+        public static Polynomial operator - (Polynomial polynomial1, Polynomial polynomial2)
+        {
+            CheckInputPolynomials(polynomial1, polynomial2);
+
+            int sizeMax = (polynomial1.Degree >= polynomial2.Degree) ? polynomial1.Degree+1 : polynomial2.Degree+1;
+            double[] arrayResult = new double[sizeMax];
+
+            for (int i = 0; i < sizeMax; i++)
+            {
+                if (i >= polynomial1.coefficientArray.Length)
+                {
+                    arrayResult[i] -= polynomial2.coefficientArray[i];
+                    continue;
+                }
+                if (i >= polynomial2.coefficientArray.Length)
+                {
+                    arrayResult[i] += polynomial1.coefficientArray[i];
+                    continue;
+                }
+                arrayResult[i] = polynomial1.coefficientArray[i] - polynomial2.coefficientArray[i];
+            }
+
+            return new Polynomial(arrayResult);
+        }
+
+        /// <summary>
+        /// Multiplies two polynomials.
+        /// </summary>
+        /// <param name="polynomial1">First polynomial.</param>
+        /// <param name="polynomial2">Second polynomial.</param>
+        /// <returns>Multiplying of two polynomials.</returns>
+        public static Polynomial operator * (Polynomial polynomial1, Polynomial polynomial2)
+        {
+            CheckInputPolynomials(polynomial1, polynomial2);
+
+            int size = polynomial1.Degree + polynomial2.Degree + 1;
+            double[] arrayResult = new double[size];
+
+            for (int i = 0; i < polynomial1.coefficientArray.Length; i++)
+            {
+                for (int j = 0; j < polynomial2.coefficientArray.Length; j++)
+                {
+                    arrayResult[i + j] += polynomial1.coefficientArray[i] * polynomial2.coefficientArray[j];
+                }
+            }
+
+            return new Polynomial(arrayResult);
+        }
+
+        /// <summary>
+        /// Checks for equality of two polynomials.
+        /// </summary>
+        /// <param name="polynomial1">First polynomial.</param>
+        /// <param name="polynomial2">Second polynomial.</param>
+        /// <returns>True, if objects are equal, and false otherwise.</returns>
+        public static bool operator == (Polynomial polynomial1, Polynomial polynomial2)
+        {
+            CheckInputPolynomials(polynomial1, polynomial2);
+            return polynomial1.Equals(polynomial2);
+        }
+
+        /// <summary>
+        /// Checks for inequality of two polynomials.
+        /// </summary>
+        /// <param name="polynomial1">First polynomial.</param>
+        /// <param name="polynomial2">Second polynomial.</param>
+        /// <returns>True, if objects aren't equal, and false otherwise.</returns>
+        public static bool operator != (Polynomial polynomial1, Polynomial polynomial2)
+        {
+            CheckInputPolynomials(polynomial1, polynomial2);
+            return !polynomial1.Equals(polynomial2);
+        }
+        #endregion
+
+        /// <summary>
+        /// Checks for equality.
+        /// </summary>
+        /// <param name="polinom">One of the polynomials.</param>
+        /// <returns>True, if polynomials are equal, and false otherwise.</returns>
+        public bool Equals(Polynomial polinom)
+        {
+            if (ReferenceEquals(polinom, null)) return false;
+            if (ReferenceEquals(polinom, this)) return true;
+            return EqualsCoefArray(polinom.coefficientArray);
+        }
+
+        /// <summary>
+        /// Calculates polynomial by given value.
+        /// </summary>
+        /// <param name="value">Double value for substituting.</param>
+        /// <returns>Double result of calculation.</returns>
+        public double CalculateByValue(double value)
+        {
+            double result = 0;
+            for (int i = 0; i < this.coefficientArray.Length; i++)
+            {
+                result += this.coefficientArray[i] * Math.Pow(value, i);
+            }
+            return result;
+        }
+        #endregion
+
+        #region private methods
+        private bool EqualsCoefArray(СoefficientArray inputCoefArray)
+        {
+            if (this.coefficientArray.Length != inputCoefArray.Length)
+                return false;
+            for(int i = 0; i < inputCoefArray.Length; i++)
+            {
+                if (inputCoefArray[i] != this.coefficientArray[i])
+                    return false;
+            }
+            return true;
+        }
+
+        private static void CheckInputPolynomials(Polynomial polynomial1, Polynomial polynomial2)
+        {
+            if (ReferenceEquals(polynomial1, null) && ReferenceEquals(polynomial2, null)) throw new ArgumentNullException($"{ nameof(polynomial1) } and { nameof(polynomial2) } are null.");
+            if (ReferenceEquals(polynomial1, null)) throw new ArgumentNullException($"{ nameof(polynomial1) } is null.");
+            if (ReferenceEquals(polynomial2, null)) throw new ArgumentNullException($"{ nameof(polynomial2) } is null.");
+        }
+        #endregion
+    }
+}
